52b2f0fd1dc59ef50b94c0cc94d64b3f218c40de
student-shweta <161452096+student-shweta@users.noreply.github.com>
2024-07-09 12:40:02 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+function openTab(tabName,buttonname) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].classList.remove("active");
+    }
+    document.getElementById(tabName).style.display = "block";
+    document.getElementById(buttonname).classList.add("active");
+}
+
+// Open the default tab
+document.addEventListener("DOMContentLoaded",function(){
+    openTab('tab2','buttonid_two');
+})
+
+
